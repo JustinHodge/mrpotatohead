ec9e0af89e39c265bccb6cf974c2ebce3b923a47
JustinHodge <51083905+JustinHodge@users.noreply.github.com>
2022-02-16 20:34:31 -0500

    more setup and testing

---
+++ src/ts/test.ts
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import {ObjectLoader} from 'three'
+
+let camera: any, scene : any, renderer: any;
+let geometry, material, mesh: any;
+
+init();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+    const loader = new ObjectLoader();
+    loader.load(
+        "./assets/10201_Potato_v1-L3.obj",
+        function (obj) {
+            scene.add(obj);
+        }
+    )
+
+    mesh = new THREE.Mesh( geometry, material );
+    // scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    // renderer.setAnimationLoop( animation );
+    document.body.appendChild( renderer.domElement );
+
+}
+
+function animation( time: any ) {
+
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
+
+    renderer.render( scene, camera );
+
+}
